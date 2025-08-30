4b1a01dd8ffa73dc65a6749be16d7f21afc81fe9
belaid1meriem <mm_belaid@esi.dz>
2025-08-30 13:41:43 +0100

    Fix domain configuration in environment variables and update API client baseURL construction

--- src/api/apiClientSubDomain.ts
+++ src/api/apiClientSubDomain.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_PROTOCOL+'://'+ localStorage.getItem('subdomain')+import.meta.env.VITE_DOMAIN+':'+import.meta.env.VITE_PORT+'/' + 'api',
+  baseURL:`${import.meta.env.VITE_PROTOCOL}://${localStorage.getItem('subdomain')}.${import.meta.env.VITE_DOMAIN}:${import.meta.env.VITE_PORT}/api`,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 })
